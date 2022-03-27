95071b824bd5143f57957857b0ad35859824c44b
Cristian Ismael Gramajo <cristiangramajo015@gmail.com>
2022-03-26 21:47:38 -0300

    Se agrega boton reset
    boton reset con funcion proveniente de customhook

--- src/Counter.js
+++ src/Counter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-export const Counter = React.memo(({ id, init, increment }) => {
+export const Counter = React.memo(({ id, init, increment, reset }) => {
   console.log(`Se renderiza Contador #${id}`);
   return (
     <fieldset style={{ margin: 0, textAlign: 'center' }}>
@@ -7,6 +7,7 @@ export const Counter = React.memo(({ id, init, increment }) => {
         ||| Contador #{id} - Valor: {init} |||
       </legend>
       <button onClick={increment}>+</button>
+      <button onClick={reset}>Restart</button>
     </fieldset>
   );
 });
